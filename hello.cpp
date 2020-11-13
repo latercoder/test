fcea6379ab955a92510b731bdc3c01b98f921945
latercoder <weilaihktk@163.com>
2020-11-13 15:54:11 +0800

    update with "endl"

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    cout << "hello world";
+    cout << "hello world" << endl;
     cout << "this is a test for add a line in hello.cpp";
     int a;
     cin >> a;
